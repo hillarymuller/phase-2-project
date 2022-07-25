6a05ad38fe3d3b3fd53eaf5de4e136129bbab2aa
Hillary Muller <hillaryaward@gmail.com>
2022-07-25 16:31:49 -0500

    add info to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function Home() {
     return (
-        <h2>Home</h2>
+        <div>
+        <h3>Time for a Vacation, Baby!</h3>
+        <h4>Rent everything you need for your trip to stop spending money on things they'll outgrow!</h4>
+        <img src="https://images.unsplash.com/photo-1472586662442-3eec04b9dbda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fGJlYWNoJTIwYmFieXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" alt="Toddler playing in the ocean."/>
+        </div>
     )
 }
 
